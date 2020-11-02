7ca36a9bc5f79c67a18bf71f47e93c02941117b1
bbawuah <brianbawuah@icloud.com>
2020-11-02 12:58:13 +0100

    add ts to d3 playground

---
+++ d3-playground/index.ts
@@ -0,0 +1,4 @@
+window.onload = function () {
+  let msg: string = "Deee driee";
+  document.querySelector("h1").innerHTML = msg;
+};
---
+++ d3-playground/js/index.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    var msg = "Deee driee";
+    document.querySelector("h1").innerHTML = msg;
+};
+//# sourceMappingURL=index.js.map
