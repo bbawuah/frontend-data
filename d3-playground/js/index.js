b0eb0828b02b06e90692d7f10e084963ced8ec09
bbawuah <brianbawuah@icloud.com>
2020-11-05 10:27:15 +0100

    fixed bar chart

--- d3-playground/js/index.js
+++ d3-playground/js/index.js
@@ -36,29 +36,41 @@ var PaymentMethods;
     renderD3(formattedArray);
 })();
 function renderD3(parkingData) {
-    const yScale = d3
-        .scaleBand()
-        .domain(parkingData.map((data) => data.paymentMethodTitle))
-        .rangeRound([0, 500])
-        .padding(0.1);
-    const xScale = d3.scaleLinear().domain([0, 300]).range([0, 700]);
-    const container = d3.select("svg").classed("container", true);
-    // Bars
-    container
+    const margin = { top: 20, right: 20, bottom: 30, left: 110 }, width = 700 - margin.left - margin.right, height = 500 - margin.top - margin.bottom;
+    // set the ranges
+    const y = d3.scaleBand().range([height, 0]).padding(0.1);
+    const x = d3.scaleLinear().domain([0, 300]).range([0, width]);
+    const svg = d3
+        .select("svg")
+        .classed("container", true)
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    // format the data
+    parkingData.forEach((data) => {
+        data.areas.length = +data.areas.length;
+    });
+    // Scale the range of the data in the domains
+    x.domain([0, d3.max(parkingData, (data) => data.areas.length)]);
+    y.domain(parkingData.map((data) => data.paymentMethodTitle));
+    // append the rectangles for the bar chart
+    svg
         .selectAll(".bar")
         .data(parkingData)
         .enter()
         .append("rect")
-        .text((data) => data.paymentMethodTitle)
-        .classed("bar", true)
-        .attr("width", (data) => xScale(data.areas.length))
-        .attr("height", yScale.bandwidth())
-        .attr("x", (data) => 700 - xScale(data.areas.length))
-        .attr("y", (data) => yScale(data.paymentMethodTitle));
-    container
+        .attr("class", "bar")
+        .attr("width", (data) => x(data.areas.length))
+        .attr("y", (data) => y(data.paymentMethodTitle))
+        .attr("height", y.bandwidth());
+    // x Axis
+    svg
         .append("g")
-        .attr("transform", "translate(110,0)")
-        .call(d3.axisLeft(yScale));
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+    // y Axis
+    svg.append("g").call(d3.axisLeft(y));
 }
 function getPaymentMethods(json) {
     const array = json.map((item) => {
